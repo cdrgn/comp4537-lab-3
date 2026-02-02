a78e753fddbdcbbcbdce7e8efefd30e341cb8198
Clinton Nguyen <nguyen.clinton@outlook.com>
2026-02-01 19:27:03 -0800

    Added missing files due to iCloud problem

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 const math = require('./modules/math');
-console.log(`Hello Clinton. 5+6 is ${math.
+console.log(`Hello Clinton. 5+6 is ${math.add(5,6)}`);
+console.log(`5-6 is ${math.subtract(5,6)}`);
